8fed1092a44c552de6eabd0e43dbc74baca1803b
TeaGross <tea.grossman@gmail.com>
2025-03-05 13:57:22 +0100

    🚧 feature: add nav bar & svg-logo
    Co-Authored-By: Karin Henriksson <183386547+KarinHson@users.noreply.github.com>
    Co-Authored-By: Ellinor <ellinor.asaka@gmail.com>
    Co-Authored-By: Alexandra <183548137+axandranathalie@users.noreply.github.com>

---
+++ src/component/NavBar.vue
@@ -0,0 +1,27 @@
+<script lang="ts"></script>
+
+<template>
+  <nav>
+    <ul>
+      <li>Hem</li>
+      <li>Luftballong</li>
+      <li>Akrobatflyg</li>
+      <li>Boka</li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  font-family: $font-heading2;
+  z-index: 2;
+}
+
+//tablet
+@media screen and (min-width: 768px) {
+}
+
+//desktop
+@media screen and (min-width: 1024px) {
+}
+</style>
