c44615b2150de8ba913ba26a82c9aa83e1d8bda2
TeaGross <tea.grossman@gmail.com>
2025-03-05 16:40:29 +0100

    🚧 Feature: working on productcard
    - created product card with props
    -added products in HomeView with related image, title & paragraph

--- src/component/ProductCard.vue
+++ src/component/ProductCard.vue
@@ -1,8 +1,41 @@
 <script lang="ts" setup>
 import ReadMoreButton from "./ReadMoreButton.vue"
+
+defineProps({
+  image: String, // Bild-URL
+  title: String, // Rubrik
+  paragraph: String, // Brödtext
+})
 </script>
 
 <template>
-  <div>Här kommer produktkortet vara</div>
-  <ReadMoreButton />
+  <div class="product-card">
+    <img :src="image" :alt="title" />
+    <h3>{{ title }}</h3>
+    <p>{{ paragraph }}</p>
+    <ReadMoreButton />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+//mobil
+div {
+  width: 95%;
+  img {
+    width: 100%;
+  }
+
+  h3 {
+    font-family: $font-body;
+    font-size: $mobile-font-size-heading3;
+  }
+
+  p {
+    font-family: $font-body;
+    font-size: $mobile-font-size-paragraph;
+  }
+}
+//tablet
+
+//desktop
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
 import ProductCard from "@/component/ProductCard.vue"
+import CardImage from "/assets/images/Card_image.png"
+import AerobaticPlane from "/assets/images/aerobatic_plane.jpg"
 </script>
 
 <template>
   <main>Hem</main>
-  <ProductCard />
+  <ProductCard
+    :image="CardImage"
+    title="Upplev himlen från en ny höjd med luftballong!"
+    paragraph="Boka en oförglömlig luftballongfärd med oss och sväv bort 
+    över fantastiska landskap. En rofylld och magisk upplevelse som ger 
+    perspektiv och minnen för livet. Välkommen att boka din flygning idag!"
+  />
+  <ProductCard
+    :image="AerobaticPlane"
+    title="Följ med på en adrenalinfylld resa med akrobatflyg!"
+    paragraph="Boka din plats för en adrenalinfylld akrobatflygning och 
+    känn vinden i ansiktet när du svävar genom loopar och snabba manövrar. 
+    En häftig och vågad upplevelse som får ditt hjärta att slå snabbare. 
+    Klar för att ta ut svängarna?"
+  />
 </template>
