4a968dd6c9eda1cd3320c558ad2c6bad777225d2
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-03-06 14:09:28 +0100

    ✨(feature): add text and style on about us information

--- src/component/AboutUs.vue
+++ src/component/AboutUs.vue
@@ -1,11 +1,28 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
-    <h2>HEEEEEEEEJ</h2>
+    <main>
+        <h2>Om oss</h2>
+        <p>Välkommen till SkyVoyage – din portal till oförglömliga flygupplevelser! Vi brinner för att ge våra kunder chansen att uppleva himlen på ett unikt och spännande sätt, oavsett om det handlar om adrenalinfyllda akrobatflygningar eller rofyllda luftballongsturer.</p>
+        <p>Våra samarbetspartners är erfarna piloter och ballongförare som garanterar en trygg och oförglömlig upplevelse, oavsett vilket äventyr du väljer. Oavsett om du vill känna G-krafterna i en loop eller sväva stilla över landskapet i en luftballong, har vi något för dig.</p>
+        <p>Låt oss hjälpa dig att ta ditt nästa äventyr till nya höjder – bokning och mer information hittar du här på vår sida. Vi ser fram emot att ge dig en upplevelse du sent kommer att glömma!</p>
+    </main>
 </template>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+    main {
+        background-color: $charcoal;
+        border-radius: 2%;
+        h2 {
+        font-family: $font-heading2;
+        font-size: $mobile-font-size-heading2;
+        color: white;
+        }
+        p {
+            font-family: $font-body;
+            font-size: $mobile-font-size-paragraph;
+            color: white;
+            font-weight: 400;
+        }
+    }
 </style>
