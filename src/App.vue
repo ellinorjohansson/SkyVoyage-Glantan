529e06fdc91254a10da9fb4161b96490dfd7de47
Alexandra <al3xandra92@hotmail.com>
2025-03-07 14:19:56 +0100

    🎉 :build: mobile bottom navbar with icons and responsiveness

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import { RouterView } from "vue-router"
 import MainHeader from "./component/MainHeader.vue"
 import MainFooter from "./component/MainFooter.vue"
+import NavBarMobile from "./component/NavBarMobile.vue";
 </script>
 
 <template>
   <MainHeader />
   <RouterView />
   <MainFooter />
+  <NavBarMobile />
 </template>
 
 <style lang="scss">
--- src/component/NavBarMobile.vue
+++ src/component/NavBarMobile.vue
@@ -1,1 +1,76 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+</script>
 
+<template>
+  <nav class="bottom-nav">
+    <router-link to="/" class="nav-item">
+      <i class="fa-solid fa-house"></i>
+    </router-link>
+
+    <div class="nav-item">
+      <img src="/assets/images/balloon_icon_orange.png" alt="Luftballong" class="icon" />
+    </div>
+
+    <div class="nav-item">
+      <img src="/assets/images/airplane_icon_orange.png" alt="Flygplan" class="icon" />
+    </div>
+
+    <router-link to="/boka" class="nav-item">
+      <i class="fa-solid fa-calendar-week"></i>
+    </router-link>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.bottom-nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: $paynes-gray;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-top: 2px solid $charcoal; 
+  z-index: 100;
+  padding: 0;
+}
+
+.icon {
+  width: 24px; 
+  height: 24px;
+  object-fit: contain; 
+}
+
+.nav-item {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  padding: 8px;
+  background-color: $paynes-gray;
+}
+
+.nav-item i, .nav-item img {
+  color: $coral; 
+  font-size: 34px;
+  width: 34px; 
+  height: 34px;
+}
+
+.nav-item:not(:last-child) {
+  border-right: 3px solid $charcoal; 
+  height: 40px;
+}
+
+
+@media screen and (min-width: 768px) {
+  .bottom-nav {
+    display: none;
+  }
+
+}
+</style>
